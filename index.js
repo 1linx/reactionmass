f7cbfb1d8dd14d9889597b232b60c3ba933da4a3
Al Warmington <apwarmington@gmail.com>
2018-04-24 17:35:05 +0100

    Early fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var port = process.env.PORT || 3000;
+// var http = require('http').Server(app);
+// var port = process.env.PORT || 3000;
 
 var apiController = require('./controllers/apiController');
 
@@ -10,6 +10,6 @@ app.set('view engine', 'ejs');
 apiController(app);
 
 // run server, set port
-http.listen(port, function(){
-  console.log('listening on port:3000');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
